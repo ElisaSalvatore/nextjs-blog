5149d9b043baa3a24726d86a6b08a1465494591c
ElisaSalvatore <elisa.salvatore110@gmail.com>
2022-09-21 11:20:15 +0200

    Adding CSS on Post component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,9 @@ import Date from "../../components/date";
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
 
+//module css
+import utilStyles from '../../styles/utils.module.css';
+
 export async function getStaticPaths() {
     const paths = getAllPostIds();
     return {
@@ -30,13 +33,13 @@ export default function Post( {postData} ) {
                 <title>{postData.title}</title>
             </Head>
 
-            {postData.title}
-            <br/>
-            {postData.id}
-            <br/>
-            {/* Replace {postData.date} with this */}
-            <Date dateString={postData.date} />
-            <br/>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+            {/* {postData.id} */}
+            <div className={utilStyles.lightText}>
+                {/* Replace {postData.date} with this */}
+                <Date dateString={postData.date} />
+            </div>
+
             {/* To render contentHtml using dangerouslySetInnerHTML (after usign remark into lib/posts) */}
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
